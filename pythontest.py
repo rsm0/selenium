907d979562ec839416c3cfbb8bcff786ce134b33
unknown <d7mi.9@hotmail.com>
2024-10-22 14:39:33 +0300

    open link GOSI with code

--- pythontest.py
+++ pythontest.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
@@ -22,23 +21,25 @@ another1_button.click()
 time.sleep(5)
 
 another3_button = WebDriverWait(driver, 5000).until(
-    EC.presence_of_element_located((By.XPATH,"/html/body/div[15]/div[3]/div/div/button"))
+EC.presence_of_element_located((By.XPATH,"/html/body/div[15]/div[3]/div/div/button"))
 )
 driver.execute_script("arguments[0].click();", another3_button)
-time.sleep(5)
-print( "Hi" )
+time.sleep(2)
 
 another4_button = WebDriverWait(driver, 5000).until(
-    EC.element_to_be_clickable((By.XPATH,"/html/body/div[19]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div/div/span[1]/div[2]/div/div[1]/div/div/span"))
+EC.element_to_be_clickable((By.XPATH,"/html/body/div[19]/div/div/div[1]/div/div/div[2]/div/div[2]/div/div/div/span[1]/div[2]/div/div[1]/div/div/span"))
 )
 another4_button.click()
-time.sleep(5)
-print( "Hi" )
+time.sleep(2)
 
-another5_button = WebDriverWait(driver, 5000).until(
-    EC.presence_of_element_located((By.XPATH,"/html/body/div[15]/div[3]/div/div/button"))
-)
-driver.execute_script("arguments[0].click();", another5_button)
+
+text = driver.find_element(By.XPATH,"/html/body/div[20]/div/div/div/div[1]/div/div/div[2]/div/div[2]/div/div/div/div/section/div[2]/div[2]/div[1]/div/span").text
+
+print(text)
+
+
+driver.get("https://www.stats.gov.sa/ar/job-details/" + text)
+
 
 input("اضغط أي مفتاح لإغلاق المتصفح...")
 driver.quit()
